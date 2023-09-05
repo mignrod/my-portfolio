da203844391d5b41b7d3ce84c75162d167c79571
mignrod <mignrod@gmail.com>
2023-09-04 20:14:27 -0400

    falls de energia

--- scripts/my-portfolio.js
+++ scripts/my-portfolio.js
@@ -1,9 +1,9 @@
-var inputs = document.getElementsById('label:focus');
+var inputs = document.getElementsByClassName('formulary-label');
 
 for (var i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener('keyup', function() {
         if(this.value.length >= 1) {
-            this.nextElementSibling.classList.add('fix');
+            this.classList.add('fix');
         } else {
             this.nextElementSibling.classList.remove('fix');
         }
