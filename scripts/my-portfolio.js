932bca0e56fcdae4cfe82e5826634092fafcf7cc
mignrod <mignrod@gmail.com>
2023-09-04 20:18:40 -0400

    resulto el js

--- scripts/my-portfolio.js
+++ scripts/my-portfolio.js
@@ -1,9 +1,9 @@
-var inputs = document.getElementsByClassName('formulary-label');
+var inputs = document.getElementsByClassName('formulary-input');
 
 for (var i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener('keyup', function() {
         if(this.value.length >= 1) {
-            this.classList.add('fix');
+            this.nextElementSibling.classList.add('fix');
         } else {
             this.nextElementSibling.classList.remove('fix');
         }
